1287056fc6bb6d3fc58bf9492909073df6fb33c3
anjomshoaa <amin.anjomshoaa@wu.ac.at>
2025-11-10 00:30:30 +0100

    Initial content added.

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,8 @@
 export const profile = {
-	fullName: 'Your Name',
-	title: '',
-	institute: '',
-	author_name: '', // Author name to be highlighted in the papers section
+	fullName: 'Amin Anjomshoaa',
+	title: 'Senior Researcher',
+	institute: 'Vienna University of Economics and Business',
+	author_name: 'Amin Anjomshoaa', // Author name to be highlighted in the papers section
 	research_areas: [
 		// { title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
 	],
@@ -10,30 +10,30 @@ export const profile = {
 
 // Set equal to an empty string to hide the icon that you don't want to display
 export const social = {
-	email: '',
-	linkedin: '',
-	x: 'https://www.x.com/',
+	email: 'amin.anjomshoaa@wu.ac.at',
+	linkedin: 'https://at.linkedin.com/in/amin-anjomshoaa-4a479227',
+	x: 'https://x.com/aminbits',
 	github: '',
 	gitlab: '',
-	scholar: '',
+	scholar: 'https://scholar.google.com/citations?user=E35-e3QAAAAJ&hl=en',
 	inspire: '',
 	arxiv: '',
-	orcid: '',
+	orcid: 'https://orcid.org/0000-0001-6277-742X',
 }
 
 export const template = {
 	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
-	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
-	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
+	lightTheme: 'emerald123', // Select one of the Daisy UI Themes or create your own
+	darkTheme: 'dim', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
     base: '' // Repository name starting with /
 }
 
 export const seo = {
-	default_title: 'Astro Academia',
-	default_description: 'Astro Academia is a template for academic websites.',
+	default_title: 'Amin Anjomshoaa',
+	default_description: 'Amin Anjomshoaa academic websites.',
 	default_image: '/images/astro-academia.png',
 }

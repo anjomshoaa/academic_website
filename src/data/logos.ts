145bce68605d629f31bffee9077e2d46ec24014e
anjomshoaa <amin.anjomshoaa@wu.ac.at>
2025-11-17 00:06:59 +0100

    Logos added.

---
+++ src/data/logos.ts
@@ -0,0 +1,20 @@
+import MIT from '@/assets/logos/MIT_logo.png';
+import UK from '@/assets/logos/UK_logo.png';
+import TU from '@/assets/logos/TU_logo.png';
+import SBA from '@/assets/logos/SBA_logo.png';
+import UG from '@/assets/logos/UG_logo.png';
+import CSH from '@/assets/logos/CSH_logo.png';
+import WU from '@/assets/logos/WU_logo.png';
+import RSA from '@/assets/logos/RSA_logo.png';
+
+
+export const logos = {
+  MIT,
+  UK,
+  TU,
+  SBA,
+  UG,
+  CSH,
+  WU,
+  RSA,
+};
